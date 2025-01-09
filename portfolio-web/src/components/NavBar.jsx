ba143f842e65df8ac8fa01e863e8157ee04ebea6
Jakub Wenek <kuba.wenek@wp.pl>
2025-01-09 18:26:56 +0100

    .

--- portfolio-web/src/components/NavBar.jsx
+++ portfolio-web/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useState, useEffect } from "react";
-import logo from '../assets/logo.svg';
+import logo from '../assets/logo.png';
 import navIcon1 from '../assets/nav-icon1.svg';
 import navIcon2 from '../assets/nav-icon2.svg';
 import navIcon3 from '../assets/nav-icon3.svg';

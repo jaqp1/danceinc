fe63df53a96113d6696ab55ccebdabaa63434d6a
Jakub Wenek <kuba.wenek@wp.pl>
2025-02-07 15:18:02 +0100

    few bugs fixed

--- portfolio-app2/src/components/Banner.jsx
+++ portfolio-app2/src/components/Banner.jsx
@@ -5,6 +5,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/header-img.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import Contact from './Contact';
 
 export default function Banner() {
 
@@ -43,18 +44,25 @@ export default function Banner() {
             setDelta(500);
         }
     }
+    function scrollToContact() {
+        document.querySelector('#connect').scrollIntoView();
+    }
+
 
   return (
     <section className="banner" id="home">
         <Container>
             <TrackVisibility offset={200}>
                 {({ isVisible }) =>
-                <Row className={isVisible ? "animate__animated animate__fadeIn align-items-center" : "animate__animated animate__fadeIN align-items-center"}>
+                <Row className={isVisible ? "animate__animated animate__fadeIn align-items-center" : "animate__animated animate__fadeIn align-items-center"}>
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to our portfolio</span>
                         <h1>{'Cześć, jesteśmy Dance Inc. '}<span className="wrap">{text}</span></h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        <button onClick={() => console.log('connect')}>Skontaktuj się z nami! <ArrowRightCircle size={25}></ArrowRightCircle></button>
+                        <button onClick={() => scrollToContact()}>
+                                Skontaktuj się z nami! 
+                                
+                            <ArrowRightCircle size={25}></ArrowRightCircle></button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                     <img  src={headerImg} alt="Header Img"/>
